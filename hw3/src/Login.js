9754f57fac4ed8c4350177ec3e9db8e27cd9764e
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2021-09-30 14:03:53 -0500

    add state to the login and register

--- hw3/src/Login.js
+++ hw3/src/Login.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 
 export default function Login({setUser}) {
-    const [ username, setUsername ] = useState('')
-    function handleUsername (e) { setUsername(e.target.value) }
+    const [ formData, setFormData ] = useState({
+        username: "",
+        password: ""
+    })
     return (
         <form onSubmit={evt => evt.preventDefault()}>
             <label htmlFor="login-username">Username: </label>
-            <input type="text" value={username} onChange={handleUsername} name="login-username" id="login-username" />
+            <input type="text" value={formData.username} onChange={e => setFormData({
+                ...formData,
+                username: e.target.value
+            })} name="login-username" id="login-username" />
             <label htmlFor="login-password">Password: </label>
-            <input type="password" name="login-pasword" id="login-password" />
-            <input type="submit" value="Login" disabled={username.length === 0}/>
+            <input type="password" value={formData.password} onChange={e => setFormData({
+                ...formData,
+                password: e.target.value
+            })} name="login-pasword" id="login-password" />
+            <input type="submit" value="Login" disabled={formData.username.length === 0}/>
         </form>
     )
 }
--- hw3/src/Register.js
+++ hw3/src/Register.js
@@ -9,12 +9,25 @@ export default function Register({setUser}) {
     return (
         <form onSubmit={e => e.preventDefault()}>
             <label htmlFor="register-username">Username: </label>
-            <input type="text" value={username} onChange={e => setFormData({...formaData, username: e.target.value})} name="register-username" id="register-username" />
+            <input type="text" value={formData.username} onChange={e => setFormData({
+                ...formData,
+                 username: e.target.value
+            })} name="register-username" id="register-username" />
             <label htmlFor="register-password">Password: </label>
-            <input type="password" name="register-password" id="register-password" />
+            <input type="password" value={formData.password} onChange={e => setFormData({
+                ...formData,
+                password: e.target.value
+            })} name="register-password" id="register-password" />
             <label htmlFor="register-password-repeat">Repeat password: </label>
-            <input type="password" name="register-password-repeat" id="register-password-repeat" />
-            <input type="submit" value="Register" disabled={username.length === 0}/>
+            <input type="password" value={formData.passwordRepeat} onChange={e => setFormData({
+                ...formData,
+                passwordRepeat: e.target.value
+            })} name="register-password-repeat" id="register-password-repeat" />
+            <input type="submit" value="Register" disabled={
+                formData.username.length === 0 ||
+                formData.password.length === 0 ||
+                formData.passwordRepeat !== formData.password
+            }/>
         </form>
     )
 }
