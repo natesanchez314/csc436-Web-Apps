e634f5fde51c96226dbc3eccf6166afcc7cdbc39
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2021-10-20 16:44:36 -0500

    Set up login with db

--- hw4/src/User/Login.js
+++ hw4/src/User/Login.js
@@ -1,20 +1,32 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { StateContext } from "../context";
+import { useResource } from 'react-request-hook'
 
 export default function Login() {
     const {dispatch} = useContext(StateContext)
-    const [username, setUsername] = useState('')
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
     function handleUsername(e) { setUsername(e.target.value) }
+    function handlePassword(e) { setPassword(e.target.value) }
+    const [ user, loginUser ] = useResource((username, password) => ({
+        url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+        method: 'get'
+    }))
+    useEffect(() => {
+        if (user && user.data) {
+            if (user.data.length > 0) dispatch({type:"LOGIN", username})
+        }
+    }, [user])
     return (
         <form onSubmit={
-            e => {
+            (e) => {
                 e.preventDefault()
-                dispatch({type:"LOGIN", username})
+                loginUser(username, password)
             }} >
             <label htmlFor="login-username">Username: </label>
             <input type="text" value={username} onChange={handleUsername} id="login-username" />
             <label htmlFor="login-password">Password: </label>
-            <input type="password" name="login-pasword" id="login-password" />
+            <input type="password" value={password} onChange={handlePassword} id="login-password" />
             <input type="submit" value="Login" />
         </form>
     ) 
